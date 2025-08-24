ed3192871b02dba53e4f72f0352e40adf99bcbdf
SERHII STETSIUK <skystets@gmail.com>
2025-08-24 17:35:03 +0200

    add js functions

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,14 @@ const openMenu = document.querySelector('[data-open]');
 const closeMenu = document.querySelector('[data-close]');
 const modalMenu = document.querySelector('[data-modal]');
 const bodyLock = document.querySelector('body');
+const bodyUnlock = document.querySelector('.js-active');
 
 openMenu.addEventListener('click', toggleModal);
 closeMenu.addEventListener('click', toggleModal);
+bodyUnlock.addEventListener('click', () => {
+	bodyLock.classList.remove('lock');
+	modalMenu.classList.remove('is-active');
+});
 
 function toggleModal() {
 	modalMenu.classList.toggle('is-active');
