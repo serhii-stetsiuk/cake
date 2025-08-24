afb3bc5670c8f7543bf177067d8114e108f3de7c
SERHII STETSIUK <skystets@gmail.com>
2025-08-24 16:52:45 +0200

    Add body-lock

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
 const openMenu = document.querySelector('[data-open]');
 const closeMenu = document.querySelector('[data-close]');
 const modalMenu = document.querySelector('[data-modal]');
+const bodyLock = document.querySelector('body');
 
 openMenu.addEventListener('click', toggleModal);
 closeMenu.addEventListener('click', toggleModal);
 
 function toggleModal() {
 	modalMenu.classList.toggle('is-active');
+	bodyLock.classList.toggle('lock');
 }
